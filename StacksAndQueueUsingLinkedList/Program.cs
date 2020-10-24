768144713a134987df8c8a8aadaad5cef292660f
Aseem <aseemanand2@gmail.com>
2020-10-24 20:29:58 +0530

    [Aseem] Added code to push data to top of stack using linked list

--- StacksAndQueueUsingLinkedList/Program.cs
+++ StacksAndQueueUsingLinkedList/Program.cs
@@ -6,7 +6,13 @@ namespace StacksAndQueueUsingLinkedList
     {
         static void Main(string[] args)
         {
-            
+            /// UC 1
+            Stack stack = new Stack();
+            stack.PushToTop(70);
+            stack.PushToTop(30);
+            stack.PushToTop(56);
+            stack.Display();
+            Console.ReadKey();
         }
     }
 }
---
+++ StacksAndQueueUsingLinkedList/Stack.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StacksAndQueueUsingLinkedList
+{
+    class Stack
+    {
+        public Node top;
+
+        /// <summary>
+        /// UC 1 : Pushesthe value to top of the stack.
+        /// </summary>
+        /// <param name="value">The value.</param>
+        public void PushToTop(int value)
+        {
+            Node newNode = new Node(value);
+            if(this.top==null)
+            {                
+                newNode.next = null;
+            }
+            else
+            {
+                newNode.next = this.top;
+            }
+            this.top = newNode;
+            Console.WriteLine("{0} pushed at top of stack",value);
+        }
+
+        /// <summary>
+        /// Displays this stack contents from top to bottom.
+        /// </summary>
+        public void Display()
+        {
+            Node temp = this.top;
+            if(temp==null)
+                Console.WriteLine("Stack is empty");
+            else
+            {
+                Console.Write("\nDisplay:Stack contents from top to bottom: ");
+                while (temp!=null)
+                {
+                    Console.Write(temp.data + " ");
+                    temp = temp.next;
+                }
+            }
+        }
+    }
+}
